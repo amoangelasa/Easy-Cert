d64d076ccb2f18157c5e05f0fe4e919c09fdaa27
Amo Angel <139236806+amoangelasa@users.noreply.github.com>
2023-11-23 00:29:55 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download the script from GitHub
+curl -O https://github.com/amoangelasa/Easy-Cert/blob/main/easycert.sh
+
+# Make it executable
+chmod +x easycert.sh
+
+# Run the script
+./easycert.sh
