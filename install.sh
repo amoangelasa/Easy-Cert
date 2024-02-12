139512d9f267ec86d3a9322cd8b317c0a7e07dde
Amo Aydin <139236806+amoangelasa@users.noreply.github.com>
2024-02-12 23:21:08 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Download install.sh from GitHub
-curl -O https://raw.githubusercontent.com/amoangelasa/Easy-Cert/blob/main/easycert.sh
+curl -O https://raw.githubusercontent.com/amoangelasa/Easy-Cert/main/easycert.sh
 
 # Make install.sh executable
 chmod +x easycert.sh
