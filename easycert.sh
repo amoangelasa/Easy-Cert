c0b07a7a46e8034d5e4ca23e4c8a8fb98abc98f3
Amo Aydin <139236806+amoangelasa@users.noreply.github.com>
2024-02-12 23:09:36 +0330

    Update easycert.sh

--- easycert.sh
+++ easycert.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+# Constants
+ACME_HOME="$HOME/.acme.sh"
+PRIVATE_KEY="/root/private.key"
+FULLCHAIN_FILE="/root/cert.crt"
+
 # Update and upgrade
-apt update && apt upgrade -y
+apt update && apt upgrade -y || { echo "Error updating and upgrading"; exit 1; }
 
 # Install required packages
-apt install curl socat -y
+apt install curl socat -y || { echo "Error installing packages"; exit 1; }
 
 # Install acme.sh script
-curl https://get.acme.sh | sh
+curl https://get.acme.sh | sh || { echo "Error installing acme.sh"; exit 1; }
 
 # Set default CA server
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+"$ACME_HOME/acme.sh" --set-default-ca --server letsencrypt || { echo "Error setting default CA server"; exit 1; }
 
 # Register account with email
 read -p "Enter your email for registration: " user_email
-~/.acme.sh/acme.sh --register-account -m "$user_email"
+"$ACME_HOME/acme.sh" --register-account -m "$user_email" || { echo "Error registering account"; exit 1; }
 
 # Issue SSL certificate
 read -p "Enter your domain or subdomain: " user_domain
-~/.acme.sh/acme.sh --issue -d "$user_domain" --standalone
+"$ACME_HOME/acme.sh" --issue -d "$user_domain" --standalone || { echo "Error issuing SSL certificate"; exit 1; }
 
 # Install certificate
-~/.acme.sh/acme.sh --installcert -d "$user_domain" \
-    --key-file /root/private.key --fullchain-file /root/cert.crt
+"$ACME_HOME/acme.sh" --installcert -d "$user_domain" \
+    --key-file "$PRIVATE_KEY" --fullchain-file "$FULLCHAIN_FILE" || { echo "Error installing certificate"; exit 1; }
 
 echo "SSL certificate for $user_domain has been successfully obtained and installed!"
