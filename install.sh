045a3e7616c8a28968f2d22a105c665583c0878a
Amo Aydin <139236806+amoangelasa@users.noreply.github.com>
2024-02-12 23:15:45 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Download the script from GitHub
-curl -O https://github.com/amoangelasa/Easy-Cert/blob/main/easycert.sh
+# Constants
+SCRIPT_NAME="easycert.sh"
+GITHUB_REPO="https://raw.githubusercontent.com/amoangelasa/Easy-Cert/blob/main/$SCRIPT_NAME"
 
-# Make it executable
-chmod +x easycert.sh
+# Download the script from GitHub
+curl -O "$GITHUB_REPO" || { echo "Error downloading $SCRIPT_NAME from GitHub"; exit 1; }
+
+# Make the script executable (if needed)
+chmod +x "$SCRIPT_NAME" || { echo "Error making $SCRIPT_NAME executable"; exit 1; }
 
 # Run the script
-./easycert.sh
+./"$SCRIPT_NAME" || { echo "Error running $SCRIPT_NAME"; exit 1; }
